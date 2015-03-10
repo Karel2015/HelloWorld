139d216bdd229e7cdd0956cc545f2bda988bda09
Karel2015 <774521894@qq.com>
2015-03-10 16:36:00 +0800

    增加了语句

--- HelloGit/src/com/dyr/test/HelloGit.java
+++ HelloGit/src/com/dyr/test/HelloGit.java
@@ -9,5 +9,7 @@ package com.dyr.test;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello git!!!");
+		System.out.println("增加了一条语句");
+		System.out.println("在增加了一条语句");
 	}
 }

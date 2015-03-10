f3bd827d5ef45190e3219777ec44bb3cf32cbc84
Karel2015 <774521894@qq.com>
2015-03-10 16:31:17 +0800

    the second commit,yes!!!

---
+++ HelloGit/src/com/dyr/test/HelloGit.java
@@ -0,0 +1,13 @@
+
+/*
+ * www.dyr.com
+ * Copyright (c) 2014 All Rights Reserved
+ */
+
+package com.dyr.test;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello git!!!");
+	}
+}
